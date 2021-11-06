d0f293101a1d0bdd11d2e43b4b0432df752c6c63
GitHub Action <action@github.com>
2021-11-06 06:03:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/shinichy/intelliintegrant/MyBundle.kt
+++ src/main/kotlin/com/github/shinichy/intelliintegrant/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.shinichy.intelliintegrant
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/shinichy/intelliintegrant/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shinichy/intelliintegrant/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shinichy.intelliintegrant.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shinichy.intelliintegrant.MyBundle
 
 class MyApplicationService {
 
